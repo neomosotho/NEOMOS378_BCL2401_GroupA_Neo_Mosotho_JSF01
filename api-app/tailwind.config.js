317fa86aae55e05eb62623392b34fc391374d5d6
Neo Mosotho <156676402+neomosotho@users.noreply.github.com>
2024-07-18 13:41:57 +0200

    "Updated tailwind.config.js to include content paths"

--- api-app/tailwind.config.js
+++ api-app/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+  "./index.html",
+  "./src/**/*.{html,js}"
+  ],
   theme: {
-    extend: {},
+  extend: {},
   },
   plugins: [],
-}
+  }
+  
 
